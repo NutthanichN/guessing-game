c84547e7c571e91a868bd3a774690f42974484ce
Nutthanich Narphromar <monaprom134@gmail.com>
2020-04-08 18:03:00 +0700

    Add content at index

--- server.js
+++ server.js
@@ -2,12 +2,13 @@
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
+const ejs = require('ejs');
 // Connection URL
 const url = 'mongodb://localhost:27017';
 
 // Database Name
-const dbName = 'myDB';
-const dbCollection = 'test';
+const dbName = 'guessingGameDB';
+const dbCollection = 'games';
 // Create a new MongoClient
 const client = new MongoClient(url);
 
@@ -32,11 +33,41 @@ client.connect(function(err) {
     // Get documents that match the query
     // col.find({}).toArray(function(err, docs) {
     //   assert.equal(null, err);
-    //   res.send(JSON.stringify(docs));
+    //   // res.send(JSON.stringify(docs));
     //   // client.close();
     // });
-    res.render('index');
+
+    col.findOne({state: 1}, (err, doc) => {
+      assert.equal(null, err);
+      // getDBVariable(doc);
+      var state = doc.state;
+      var question = doc.question;
+      var question = question.join(" ");
+      var guessing = doc.guessing;
+      var guessing = doc.guessing;
+      var guessing = guessing.join(" ");
+      var answer = doc.answer;
+      var score = doc.score;
+      var fail = doc.fail;
+      var step = doc.step;
+      // console.log(doc.question);
+      // console.log(doc.answer);
+      // console.log(doc.guessing);
+
+      // res.send(JSON.stringify(doc));
+      // client.close();
+      res.render('index', {
+        question: question, guessing: guessing, 
+        answer: answer, fail: fail
+      });
+
+    });
   });
+
+//   app.post('/', (req, res) => {
+//       // post request -> update database and render
+//   });
+
 });
 
 app.listen(PORT, HOST);
